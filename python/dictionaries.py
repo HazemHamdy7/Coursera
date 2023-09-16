e654fd6e79c331ba683d3d70814a702edd3ba7dc
HazemHamdy7 <HazemHamdy7.com>
2023-09-16 11:47:15 -0700

    python

---
+++ python/dictionaries.py
@@ -0,0 +1,7 @@
+dict1 = {1: 'Coffee', 2: 'Tea', 3: 'Juice'}
+
+
+dict2 = 'Mint Tea'
+# del dict1[3]
+print(dict1[1])
+
---
+++ python/set.py
@@ -0,0 +1,15 @@
+set_a = {1,2,3,4,5}
+set_b = {4,6,7,8,5}
+
+
+# set_a.add(8)
+# set_a.remove(2)
+
+print(set_a.symmetric_difference(set_b))
+print(set_a ^ set_b)
+# print(set_a.intersection(set_b))
+# print(set_a & set_b)
+
+#  this to marge 2 list in one list
+# print(set_a.union(set_b))
+# print(set_a|set_b)
