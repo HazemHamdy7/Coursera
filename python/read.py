5de3ef57e263e7a1bfc2686a29a2c31a75d9c073
HazemHamdy7 <HazemHamdy7.com>
2023-09-17 14:22:07 -0700

    reading file in python

--- python/read.py
+++ python/read.py
@@ -6,8 +6,41 @@
 # with open('test.txt' ,  'w') as file:
 #     file.writelines  (["This is a new file create!" , "\nAHazm hamdy"])
 
-try:
-    with open(' test.txt' ,  'w') as file:
-        file.writelines  (["\nThis is a new file create!" , "\nAHazm hamdy"])   
-except FileNotFoundError as e:
-  print("Errorrrrrrrrr",e )  
+# try:
+#     with open(' test.txt' ,  'w') as file:
+#         file.writelines  (["\nThis is a new file create!" , "\nAHazm hamdy"])   
+# except FileNotFoundError as e:
+#   print("Errorrrrrrrrr",e )  
+
+# with open ("petnames.txt", "r") as file:
+#     date =  file.readlines()
+#     for i in date:
+#         print(i)
+
+
+# f= open("petnames.txt", "r") 
+# f_content = f.readlines()
+# # f_content_list = f_content.split("\n")
+# print(f_content)
+
+
+# import random
+# f = open("petnames.txt", "r")
+# f_content = f.read()
+# f_content_list = f_content.split("\n")
+# print(random.choice(f_content_list))
+
+
+
+# import random
+# f = open("petnames.txt", "r")
+# f_cont= f.read()
+# f_cont_list = f_cont.split("\n")
+# print(random.choice(f_cont_list))
+
+import random
+f_name = input('hazem.txt: ')
+f = open('r') # "r" omitted as it's the default
+f_content = f.read()
+f_content_list = f_content.split("\n")
+print(random.choice(f_content_list))
