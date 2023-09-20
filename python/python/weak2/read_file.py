4b1e84d5feb282396f36a52128c694149ca6fe75
HazemHamdy7 <HazemHamdy7.com>
2023-09-20 22:15:30 +0200

    read file part two in python

--- python/python/weak2/read_file.py
+++ python/python/weak2/read_file.py
@@ -32,10 +32,38 @@
 
 # # ? to exception file  
 
-try :
-    with open('dd/newfile.txt', mode='a') as file:
-        file.writelines(["\nHello there My name Hazem",
-                        "\nHello there My name Hazem", 
-                       "\nHello there My name Hazem"])
-except  FileNotFoundError as e :
-    print("Error" , e)
+# try :
+#     with open('dd/newfile.txt', mode='a') as file:
+#         file.writelines(["\nHello there My name Hazem",
+#                         "\nHello there My name Hazem", 
+#                        "\nHello there My name Hazem"])
+# except  FileNotFoundError as e :
+#     print("Error" , e)
+
+
+
+# import random
+# f = open("petnames.txt", "r")
+# f_content= f.read()
+# f_content_list = f_content.split("\n")
+# print(random.choice(f_content_list))
+
+
+# import random
+
+# f_name = input('Type the file name: ')
+
+# f = open(f_name,'r') # "r" omitted as it's the default
+# f_content = f.read()
+# f_content_list = f_content.split("\n")
+# print(random.choice(f_content_list))
+
+
+import random
+
+f_name = input("Taye thr file name : " )
+f = open(f_name,'r')
+f_content = f.read()
+f_content_list = f_content.split("\n")
+
+print(random.choice(f_content_list))
